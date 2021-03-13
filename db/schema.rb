0e09495d5d9da5528f282a19b1ec3c10619852a0
vullbyte <oto10tis@outlook.jp>
2021-03-13 08:55:58 +0000

    [Finish] lesson8

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_083025) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
